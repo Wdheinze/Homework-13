6205642425abffaf80d111834405f8a60571de7c
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-04-11 21:14:43 -0600

    Update 3js.js

--- 3js.js
+++ 3js.js
@@ -6,16 +6,33 @@ var camera = new THREE.PerspectiveCamera(
     1000
 );
 
+//This is how I make OBJECTS
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var geometry2 = new THREE.TorusGeometry(.9, .1, 19, 150);
+var material2 = new THREE.MeshBasicMaterial({
+    color: 0xe0ee00
+});
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({
     color: 0x00ff00
 });
 var cube = new THREE.Mesh(geometry, material);
+var cube2 = new THREE.Mesh(geometry2, material2);
 scene.add(cube);
+scene.add(cube2);
+
+//Dont make objects under this point
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 4
+pointLight.position.z = 4
+scene.add(pointLight)
+
 
 camera.position.z = 5;
 
@@ -23,6 +40,8 @@ function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    cube2.rotation.x += 0.03;
+    cube2.rotation.y += 0.03;
     renderer.render(scene, camera);
 }
 animate();
